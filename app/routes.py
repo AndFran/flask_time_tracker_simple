faf72c9b9eacfd0eb23432035dcac3a6f6aea513
= <=>
2018-11-07 13:42:30 +0100

    registration and login forms

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import flask_app, db
+from app.forms import LoginForm, RegisterForm
 from flask import render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User
@@ -19,9 +20,30 @@ def index():
 
 @flask_app.route("/login")
 def login():
-    return render_template('login.html')
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+
+    return render_template('login.html', form=form)
 
 @flask_app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@flask_app.route("/register")
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+
+    return render_template('register.html', form=form)
+
+
+
+
+
+
+
